79650081113d4f21539246e5eab284caf4028667
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-30 10:52:55 +0100

    Create startqeme.sh

---
+++ startqeme.sh
@@ -0,0 +1,6 @@
+qemu-system-x86_64 -machine q35 -m 1024 -smp cpus=2 -cpu qemu64 \
+-drive if=pflash,format=raw,read-only,file=$PREFIX/share/qemu/edk2-x86_64-code.fd \
+-netdev user,id=n1,\
+hostfwd=tcp::9000-:9000,\
+hostfwd=tcp::2222-:22 -device virtio-net,netdev=n1 \
+-nographic alpine.img
