270eade9c2f5fd630ed60d03671e9fba72355381
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-30 10:59:45 +0100

    Update setup_portainer.sh

--- setup_portainer.sh
+++ setup_portainer.sh
@@ -2,4 +2,6 @@ cd ~/alpine &&
 # Backup
 mv startqemu.sh bk_startqemu.sh &&
 # curl amended version
-curl -o startqemu.sh https://raw.githubusercontent.com/mrp-yt/docker_and_portainer_on_dex/main/startqeme.sh
+curl -o startqemu.sh https://raw.githubusercontent.com/mrp-yt/docker_and_portainer_on_dex/main/startqeme.sh &&
+chmod 755 startqemu.sh &&
+rm ~/setup_portainer.sh
