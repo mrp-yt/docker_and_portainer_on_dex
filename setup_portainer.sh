4900f7cd4686b0cb0be23e5cfac8cb0c66802273
mrp-yt <88549505+mrp-yt@users.noreply.github.com>
2021-08-30 10:53:56 +0100

    Create setup_portainer.sh

---
+++ setup_portainer.sh
@@ -0,0 +1,5 @@
+cd /alpine &&
+# Backup
+mv startqemu.sh bk_startqemu.sh &&
+# curl amended version
+curl -o startqemu.sh https://raw.githubusercontent.com/mrp-yt/docker_and_portainer_on_dex/main/startqeme.sh
